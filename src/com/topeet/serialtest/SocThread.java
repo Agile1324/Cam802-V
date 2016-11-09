eeba921e57befa1f6937ea7ff9f615c6e1f47eec
Agile1324 <lie3129335@gmail.com>
2016-11-09 17:00:40 +0800

    read(buffer)
    已经读取到byte[]数组，但有时会丢失数据。常规备份

--- src/com/topeet/serialtest/SocThread.java
+++ src/com/topeet/serialtest/SocThread.java
@@ -33,7 +33,7 @@ public class SocThread extends Thread{
 	BufferedReader reader = null;
 	BufferedWriter writer = null;
 	
-	
+	private InputStream mInputStream;
 	private String tag = "socket thread";
 	
 	Handler inHandler;//输入异步任务
@@ -61,8 +61,9 @@ public class SocThread extends Thread{
 			socket = new Socket();  
 			try {
 				socket.connect(new InetSocketAddress("192.168.11.123", 2001), 5000);
-				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				startWifiReplyListener(reader);
+				//reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				//startWifiReplyListener(reader);
+				
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -76,45 +77,81 @@ public class SocThread extends Thread{
 			}//接收数据
 			
 	}
+	/**
+	 * 暂时无法启动
+	 */
+	public void run(){
+		String[] Receive_date = null ;
+		conn();
+		int i_hex = 0;
+		while(true){
+			InputStream in;
+			try {
+				in = socket.getInputStream();
+				byte[] buffer = new byte[in.available()];
+				while(in.read(buffer) > 0){
+					for(int i = 0 ; i< buffer.length ; i++){
+						String hex = Integer.toHexString(buffer[i] &0xff);
+						
+						Log.d(tag, hex);
+						if(hex.length() == 1){
+							hex = "0" +hex;
+						}else{
+							Receive_date[i] = hex ;
+						}
+						i_hex = Integer.valueOf(Receive_date[4] , 16);
+						
+						switch(i_hex){
+						
+						case 20:
+							Log.d(tag, "数字为20 时触发"+hex);
+						}
+						
+					}
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+	}
 
 
+
+	/*
 	private void startWifiReplyListener(final BufferedReader reader) {
 		new Thread(new Runnable() {
 			@Override
 			public void run() {
-				try {
-					byte response ;    
-					
-					int [] rx = new int [40]; 
-					int i = 0; 
 					//while ((response = reader.readLine()) != null){
-					while ((response = (byte) reader.read()) != -1 ){ //单个读取
-						rx[i++] = response;
-						
-						if(rx[0] == 3d){
-							i=0;
-							Log.d(tag, "打印第er行数据");
+					while (!isInterrupted()){ 
+						int size;
+						try {
+						if(reader == null )return;
+							size = reader.read();
+							if(size > 0 ){
+								Log.d(tag, "打印数据");
+							}
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
 						}
-						
 						
 						Log.d(tag, "Data 已读取" + response);
 						Message msg = inHandler.obtainMessage();
 						msg.obj = response;
 						inHandler.sendMessage(msg);
 					}
-				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
 			}
 		}).start();
-	}
+	}*/
 	
 	
-	
+
 	/**
 	 * 实时接受数据
-	 */
+	 *
 	@Override
 	public void run() {
 		//打开本地socket串口
@@ -150,18 +187,18 @@ public class SocThread extends Thread{
 	}
 	
 	/**
-	 * 关闭连接
+	 * 关闭连接 暂时不用
 	 */
 	public void close() {
-		try {
-			if (socket != null) {
-				//in.close();
-				out.close();
-				socket.close();
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+		//try {
+		//	if (socket != null) {
+		//		//in.close();
+		//		out.close();
+		//	socket.close();
+		//	}
+		//} catch (Exception e) {
+		//	e.printStackTrace();
+		//}
 
 	}
 	
